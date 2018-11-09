38e567341c3ded44d5367896b864591ebfe33e40
Paul Boussard <boussard.paul@hotmail.com>
2018-11-09 11:14:50 +0100

    Main

---
+++ Test.java
@@ -0,0 +1,19 @@
+class Test{
+  public static void main(String[] args){
+    Centrale centre;
+    centre = new Centrale();
+
+    int ref = 1;
+    double valmin = 10;
+    double valmax = 20;
+    CP cp;
+    cp = new CP(centre, ref, valmin, valmax);
+
+    int ref2 = 2;
+    TB tb;
+    tb = new TB(cp, centre, ref2);
+
+    centre.demandeValeur(cp);
+    tb.demandeMesure();
+  }
+}
