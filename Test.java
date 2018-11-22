334fab425316c65bd3b99094e11198836e9cb0a3
Paul Boussard <boussard.paul@hotmail.com>
2018-11-22 08:25:19 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -13,8 +13,9 @@ class Test{
     TB tb;
     tb = new TB(cp, centre, ref2);
 
+    centre.addTB(tb);
+
     centre.timer();
-    //centre.addTB(tb);
     //centre.demandeValeur(cp);
     //tb.demandeMesure();
     tb.demandeLastValeur();
