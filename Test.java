c94a5a7099009d28d9c684e2fa21c3baa7b893b2
Paul Boussard <boussard.paul@hotmail.com>
2018-11-22 10:29:13 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -10,20 +10,10 @@ class Test{
     cp = new CP(centre, ref, valmin, valmax);
 
     int ref2 = 2;
-    int time = 10000;
+    int time = 5000;
     TB tb;
     tb = new TB(cp, time, centre, ref2);
 
-    int ref3 = 10;
-    double valmin1 = 10;
-    double valmax1 = 20;
-    CP cp2;
-    cp2 = new CP(centre, ref3, valmin1, valmax1);
-
-    int ref4 = 2;
-    int time1 = 5000;
-    TB tb2;
-    tb2 = new TB(cp2, time1, centre, ref4);
 
   }
 }
