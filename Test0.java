2cd0e4710a54c710dc3266aefd2c0a0db6dc29ab
Paul Boussard <boussard.paul@hotmail.com>
2018-11-27 11:55:42 +0100

    coucou

---
+++ Test0.java
@@ -0,0 +1,32 @@
+class Test{
+  public static void main(String[] args){
+    Centrale centre;
+    centre = new Centrale();
+
+    int ref = 1;
+    double valmin = 10;
+    double valmax = 20;
+    CP cp;
+    cp = new CP(centre, ref, valmin, valmax);
+
+    int ref2 = 1;
+    int time = 10000;
+    TB tb;
+    tb = new TB(cp, time, centre, ref2);
+
+    int ref3 = 2;
+    double valmin1 = 10;
+    double valmax1 = 20;
+    CP cp2;
+    cp2 = new CP(centre, ref3, valmin1, valmax1);
+
+    int ref4 = 2;
+    int time1 = 20000;
+    TB tb2;
+    tb2 = new TB(cp2, time1, centre, ref4);
+
+    tb.demandeMesure();
+    tb.demandeLastValeur();
+
+  }
+}
